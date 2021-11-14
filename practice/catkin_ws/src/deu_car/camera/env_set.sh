312b85a87f651bb4825c427f910d6aecbaae0b8e
parkSungHo <56144682+Hoouoo@users.noreply.github.com>
2021-11-14 23:42:55 +0900

    Delete practice/catkin_ws/src/deu_car directory

--- practice/catkin_ws/src/deu_car/camera/env_set.sh
+++
@@ -1,6 +0,0 @@
-#sudo mv /opt/ros/kinetic/share/kobuki_description/urdf/kobuki_gazebo.urdf.xacro /opt/ros/kinetic/share/kobuki_description/urdf/origin.kobuki_gazebo.urdf.xacro
-#sudo mv /opt/ros/kinetic/share/kobuki_description/urdf/kobuki.urdf.xacro /opt/ros/kinetic/share/kobuki_description/urdf/origin.kobuki.urdf.xacro
-
-sudo cp kobuki_gazebo.urdf.xacro /home/rosuser/catkin_ws/src/kobuki_description/urdf/
-sudo cp kobuki.urdf.xacro /home/rosuser/catkin_ws/src/kobuki_description/urdf/
-
